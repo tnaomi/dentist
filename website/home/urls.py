e31a4faeee8bc110b5fdd0ef8fab111652870a6b
tnaomi <naomikapengule@gmail.com>
2022-09-15 21:55:02 +0200

    Set Up

--- website/home/urls.py
+++ website/home/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path,include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns=[
     path('home/',views.home, name='home'),
@@ -11,4 +14,4 @@ urlpatterns=[
     path('blog/',views.blog,name='blog'),
     path('pricing/',views.pricing,name='pricing'),
     path('blog/<int:yr>/<str:mon>/<int:dy>/',views.blog_single,name='blog-n'),
-]
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
