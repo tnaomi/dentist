db48ad6ac1f49faedd78b1d4a9a331b79b24e593
tnaomi <naomikapengule@gmail.com>
2022-09-22 13:31:10 +0200

    changes to settings.py

--- website/website/settings.py
+++ website/website/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
